3080c54788c52f0be5ebce77fb35da5ff3810055
MikeHite <mike.p.hite@gmail.com>
2016-12-30 18:02:26 -0500

    Changed the parameters to reflect ace setting

--- ui/mission_params.hpp
+++ ui/mission_params.hpp
@@ -10,7 +10,7 @@ class Params
 		title = $STR_PARAMS_UNITCAP;
 		values[] = {0.5,0.75,1,1.25,1.5,2};
 		texts[] = {$STR_PARAMS_UNITCAP1,$STR_PARAMS_UNITCAP2,$STR_PARAMS_UNITCAP3,$STR_PARAMS_UNITCAP4,$STR_PARAMS_UNITCAP5,$STR_PARAMS_UNITCAP6};
-		default = 1;
+		default = 1.5;
 	};
 	class Difficulty {
 		title = $STR_PARAMS_DIFFICULTY;
@@ -22,31 +22,31 @@ class Params
 		title = $STR_AGGRESSIVITY_PARAM;
 		values[] = {0.25,0.5,1,2,4};
 		texts[] = {$STR_AGGRESSIVITY_PARAM0, $STR_AGGRESSIVITY_PARAM1,$STR_AGGRESSIVITY_PARAM2,$STR_AGGRESSIVITY_PARAM3,$STR_AGGRESSIVITY_PARAM4};
-		default = 1;
+		default = 2;
 	};
 	class AdaptToPlayercount{
 		title = $STR_PARAM_ADAPT_TO_PLAYERCOUNT;
 		values[] = {1,0};
 		texts[] = {$STR_PARAMS_ENABLED,$STR_PARAMS_DISABLED};
-		default = 1;
+		default = 0;
 	};
 	class DayDuration {
 		title = $STR_PARAMS_DAYDURATION;
 		values[] = { 48, 24, 16, 12, 9.6, 8, 6.8, 6, 4.8, 4, 3, 2.4, 2, 1.6, 1, 0.66, 0.5, 0.375, 0.25, 0.1875, 0.125, 0.11 };
 		texts[] = { "0.5", "1", "1.5", "2", "2.5", "3","3.5","4","5","6","8","10","12","15","24","36","48","64","96","128","192","240" };
-		default = 12;
+		default = 3;
 	};
 	class ShorterNights {
 		title = $STR_SHORTER_NIGHTS_PARAM;
 		values[] = {1,0};
 		texts[] = { $STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED };
-		default = 0;
+		default = 1;
 	};
 	class Weather {
 		title = $STR_WEATHER_PARAM;
 		values[] = { 1,2,3 };
 		texts[] = { $STR_WEATHER_PARAM1, $STR_WEATHER_PARAM2, $STR_WEATHER_PARAM3 };
-		default = 3;
+		default = 0;
 	};
 	class ResourcesMultiplier {
 		title = $STR_PARAMS_RESOURCESMULTIPLIER;
@@ -76,13 +76,13 @@ class Params
 		title = $STR_PARAMS_CIVILIANS;
 		values[] = {0,0.5,1,2};
 		texts[] = {$STR_PARAMS_CIVILIANS1,$STR_PARAMS_CIVILIANS2,$STR_PARAMS_CIVILIANS3,$STR_PARAMS_CIVILIANS4};
-		default = 1;
+		default = 0;
 	};
 	class TeamkillPenalty{
 		title = $STR_PARAM_TEAMKILL_PENALTY;
 		values[] = {1,0};
 		texts[] = { $STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED };
-		default = 0;
+		default = 1;
 	};
 	class PassiveIncome{
 		title = $STR_PARAM_PASSIVE_INCOME;
@@ -94,7 +94,7 @@ class Params
 		title = $STR_AMMO_BOUNTIES;
 		values[] = {1,0};
 		texts[] = { $STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED };
-		default = 1;
+		default = 0;
 	};
 	class CivPenalties{
 		title = $STR_CIV_PENALTIES;
@@ -154,19 +154,19 @@ class Params
 		title = $STR_CLEANUP_PARAM;
 		values[] = { 0,1,2,4 };
 		texts[] = { $STR_PARAMS_DISABLED, $STR_CLEANUP_PARAM1, $STR_CLEANUP_PARAM2, $STR_CLEANUP_PARAM3 };
-		default = 2;
+		default = 1;
 	};
 	class Introduction {
 		title = $STR_PARAMS_INTRO;
 		values[] = { 1, 0 };
 		texts[] = { $STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED };
-		default = 1;
+		default = 0;
 	};
 	class DeploymentCinematic {
 		title = $STR_PARAMS_DEPLOYMENTCAMERA;
 		values[] = { 1, 0 };
 		texts[] = { $STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED };
-		default = 1;
+		default = 0;
 	};
 	class FirstFob {
 		title = $STR_PARAMS_FIRSTFOB;
@@ -198,4 +198,30 @@ class Params
 		texts[] = {"No","Disable for clients without local AIs","Disable for all clients"};
 		default = 0;
 	};
+		class Spacer3 {
+		title = "";
+		values[] = { "" };
+		texts[] = { "" };
+		default = "";
+	};
+	class MedicalOptions{
+		title = "ACE Medical settings";
+		values[] = { "" };
+		texts[] = { "" };
+		default = "";
+	};
+	class ace_medical_level { //This needs to match an ace_setting, this one is a "SCALAR"(number)
+        title = "Medical Level"; // Name that is shown
+        ACE_setting = 1; //Marks param to be read as an ace setting, without this nothing will happen!
+        values[] = {1, 2}; //Values that ace_medical_level can be set to
+        texts[] =  {"Basic", "Advanced"}; //Text names to show for values (Basic will set level to 1, Advanced will set level to 2)
+        default = 2; //Default value used - Value should be in the values[] list
+    };
+	class ace_medical_AIDamageThreshold { //This needs to match an ace_setting, this one is a "SCALAR"(number)
+        title = "Ain't no rest to the wicked (20% more hitpoints for AI)"; // Name that is shown
+        ACE_setting = 1; //Marks param to be read as an ace setting, without this nothing will happen!
+        values[] = {1, 1.2}; //Values that ace_medical_level can be set to
+        texts[] =  {"No", "Yes"}; //Text names to show for values (Basic will set level to 1, Advanced will set level to 2)
+        default = 1; //Default value used - Value should be in the values[] list
+    };
 };
